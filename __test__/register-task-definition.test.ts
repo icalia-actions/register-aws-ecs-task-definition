6a0fd82e2bc1de93815e82a13b1cf4e30aa8a5d7
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2021-04-10 05:52:27 +0000

    Dummy test

---
+++ __test__/register-task-definition.test.ts
@@ -0,0 +1,12 @@
+// import * as fs from 'fs';
+// import * as path from 'path';
+// import * as semver from 'semver';
+
+// import * as main from '../src/main';
+
+describe('registerTaskDefinition', () => {
+  it('passes', async () => {
+    console.log('dummy test...');
+    expect(true).toEqual(true);
+  });
+});
